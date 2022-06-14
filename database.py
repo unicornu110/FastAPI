8fdc37a1dcb31f37f2ecfcf246b64fccceec509b
unicornu110 <107338077+unicornu110@users.noreply.github.com>
2022-06-14 16:06:27 +0530

    Update database.py

--- database.py
+++ database.py
@@ -9,6 +9,6 @@ engine = _sql.create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread":False}
 )
 
-SessionLocal = _orm.create_session(autocommit = False, autoflush = False, bind = engine)
+SessionLocal = _orm.sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = _declarative.declarative_base()
